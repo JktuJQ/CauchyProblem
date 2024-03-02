31e54f22537433c00539fc8b13306688525fcf28
JktuJQ <odmamontov@gmail.com>
2024-03-03 01:50:16 +0300

    Implemented plotting
    Implemented plotting over two-dimensional functions and data arrays.
    Tried it on a following task:
        x0(0) = 5.0, y0(0) = 7.0
        dx/dt = 2x + y
        dy/dt = 3x + 4y
    and compared it with analytical solution for x.

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,31 @@
 module Main where
 
+import Data.Array(listArray, (!))
+
+import Plotting
+import NumericalMethods.Euler
+import Times
+
 main :: IO ()
 main = do
+    let plot_original = let original_solution t = (2.0 * exp t) + (3.0 * exp (5 * t))
+                        in plotFn2D "original-solution" (0.0, 1.0) original_solution
+    _ <- savePlot2D "plots/x-original-solution.png" [plot_original]
+    
+    let plot_numerical = let (t0, t1, tau) = (0.0, 1.0, 0.01)
+                             (x0, y0) = (5.0, 7.0)
+                             x_eq _ vars = (2.0 * (vars!'x')) + (vars!'y')
+                             y_eq _ vars = (3.0 * (vars!'x')) + (4.0 * (vars!'y'))
+                             conditions = [(x0, x_eq), (y0, y_eq)]
+                             problem = listArray ('x', 'y') conditions
+
+                             timegrid = createTimegrid (t0, t1) tau
+
+                             (timeline, result) = methodExplicit timegrid problem
+
+                             x_numerical_solution = zip timeline [step!'x' | step <- result]
+                         in plotData2D "numerical-solution" (Just WithBars) x_numerical_solution
+    _ <- savePlot2D "plots/x-numerical-solution.png" [plot_numerical]
+    
+    _ <- savePlot2D "plots/x-both-solutions.png" [plot_original, plot_numerical]
     print ""
---
+++ app/NumericalMethods.hs
@@ -0,0 +1,23 @@
+{-
+ `NumericalMethods` module declares functions that are used in most of numerical methods.
+-}
+module NumericalMethods where
+
+import CauchyProblem
+import Times
+
+{-
+ `IterMethod` type represents functions which implement iterative numerical methods.
+
+ Iterative numerical method should be inductive,
+ so by taking previous state and new time it should be able to produce new state.
+-}
+type IterMethod = Vars -> Time -> Vars
+
+{-
+ `stepIter` function inductively calculates result of iterative numerical method
+  by iterating on a `Timeline`.
+-}
+stepIter :: IterMethod -> Vars -> Time -> Timeline -> [Vars]
+stepIter f prev_step t [] = [f prev_step t]
+stepIter f prev_step t new_timeline = prev_step : stepIter f (f prev_step t) (head new_timeline) (tail new_timeline)
